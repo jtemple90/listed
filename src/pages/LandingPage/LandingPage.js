7b16bfb53a66d5ac8a99a033bd66ff56391ed291
John Temple <johntemple@Alenas-MacBook-Air.local>
2021-06-05 14:15:32 -0700

    completed hero section

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,19 +1,22 @@
 import React from 'react'
-import HeroImg from '../../images/HeroImg.jpg'
+// import HeroImg from '../../images/HeroImg.jpg'
 import { Link } from 'react-router-dom'
+import './Landing.css'
 
 const LandingPage = () => {
   return (
     <>  
     <div className="hero-container">
-      <img src={HeroImg} alt="" />
-      <h1>YOUR NEW HOME AWAITS!</h1>
-      <p>Find YOur next home</p>
+    <div className="inner-txt">
+       <h1>YOUR NEW HOME AWAITS!</h1>
+      <p>Find your next home.</p>
       <div className="hero-btns">
         <Link to='/listings'>
           <button className='login'>Available Homes</button>
         </Link>
       </div>
+    </div>
+     
     </div>
     </>
   )
